b6d939491f1f86842d7a460b4889f66b57eaa08b
Kevin Haas <93004710+Kmhaas@users.noreply.github.com>
2022-04-06 13:06:46 -0400

    Deliverable1-MechaC

--- MechCar_Challenge.R
+++ MechCar_Challenge.R
@@ -1,7 +1,13 @@
+# import library
 library(tidyverse)
 library(dplyr)
+# import and read dataset
 MechaCar <- read.csv('MechaCar_mpg.csv')
 head(MechaCar)
+#create linear model
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) 
+#summarize linear model
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar)) 
 
-lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) #create linear model
-summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar)) #summarize linear model
+
+
