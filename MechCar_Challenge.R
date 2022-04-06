d2228a0770339341a04ec585d17f1baee06fc5e3
Kevin Haas <93004710+Kmhaas@users.noreply.github.com>
2022-04-06 13:28:59 -0400

    Deliverable-2

--- MechCar_Challenge.R
+++ MechCar_Challenge.R
@@ -1,7 +1,7 @@
 # import library
 library(tidyverse)
 library(dplyr)
-# import and read dataset
+# import and read MechaCar data
 MechaCar <- read.csv('MechaCar_mpg.csv')
 head(MechaCar)
 #create linear model
@@ -10,4 +10,14 @@ lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaC
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar)) 
 
 
+# import and read Suspenion coil data
+Suspension_COil <- read.csv('Suspension_Coil.csv')
+head(Suspension_COil)
 
+# create total summary
+total_Sum <- summarise(Suspension_COil,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+total_Sum
+
+# create lot summary
+lot_Sum <- Suspension_COil %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_Sum
